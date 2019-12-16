1cda87d438a042b0708b8d8218ef4c0270be6267
huneau romain <huneau.romain@gmail.com>
2019-12-16 14:23:34 +0100

    fix when mix up recipe

--- src/main/java/gregicadditions/machines/TileEntityLargeThermalCentrifuge.java
+++ src/main/java/gregicadditions/machines/TileEntityLargeThermalCentrifuge.java
@@ -15,136 +15,185 @@ import gregtech.api.multiblock.FactoryBlockPattern;
 import gregtech.api.multiblock.PatternMatchContext;
 import gregtech.api.recipes.CountableIngredient;
 import gregtech.api.recipes.Recipe;
+import gregtech.api.recipes.RecipeBuilder;
 import gregtech.api.recipes.RecipeMaps;
 import gregtech.api.render.ICubeRenderer;
+import gregtech.api.util.GTLog;
 import gregtech.common.blocks.BlockWireCoil;
 import gregtech.common.metatileentities.multi.electric.MetaTileEntityElectricBlastFurnace;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.items.IItemHandlerModifiable;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Optional;
+import java.util.*;
 
 public class TileEntityLargeThermalCentrifuge extends RecipeMapMultiblockController {
 
-	private static final MultiblockAbility<?>[] ALLOWED_ABILITIES = {MultiblockAbility.IMPORT_ITEMS, MultiblockAbility.EXPORT_ITEMS, MultiblockAbility.INPUT_ENERGY};
+    private static final MultiblockAbility<?>[] ALLOWED_ABILITIES = {MultiblockAbility.IMPORT_ITEMS, MultiblockAbility.EXPORT_ITEMS, MultiblockAbility.INPUT_ENERGY};
 
 
-	public TileEntityLargeThermalCentrifuge(ResourceLocation metaTileEntityId) {
-		super(metaTileEntityId, RecipeMaps.THERMAL_CENTRIFUGE_RECIPES);
-		MultiThermalCentrifugeRecipeLogic multiblockRecipeLogic = new MultiThermalCentrifugeRecipeLogic(this);
-		this.recipeMapWorkable = multiblockRecipeLogic;
+    public TileEntityLargeThermalCentrifuge(ResourceLocation metaTileEntityId) {
+        super(metaTileEntityId, RecipeMaps.THERMAL_CENTRIFUGE_RECIPES);
+        MultiThermalCentrifugeRecipeLogic multiblockRecipeLogic = new MultiThermalCentrifugeRecipeLogic(this);
+        this.recipeMapWorkable = multiblockRecipeLogic;
 
-	}
+    }
 
-	@Override
-	public MetaTileEntity createMetaTileEntity(MetaTileEntityHolder holder) {
-		return new TileEntityLargeThermalCentrifuge(metaTileEntityId);
-	}
-
-
-	@Override
-	protected void formStructure(PatternMatchContext context) {
-		super.formStructure(context);
-		BlockWireCoil.CoilType coilType = context.getOrDefault("CoilType", BlockWireCoil.CoilType.CUPRONICKEL);
-	}
-
-	@Override
-	protected BlockPattern createStructurePattern() {
-		return FactoryBlockPattern.start()
-				.aisle("XXX", "XXX")
-				.aisle("XXX", "X#X")
-				.aisle("XXX", "XSX")
-				.setAmountAtLeast('L', 9)
-				.where('S', selfPredicate())
-				.where('L', statePredicate(getCasingState()))
-				.where('X', statePredicate(getCasingState()).or(abilityPartPredicate(ALLOWED_ABILITIES)))
-				.where('C', MetaTileEntityElectricBlastFurnace.heatingCoilPredicate())
-				.where('#', isAirPredicate())
-				.build();
-
-	}
-
-	public IBlockState getCasingState() {
-		return GAMetaBlocks.METAL_CASING.getState(GAMetalCasing.MetalCasingType.RED_STEEL_CASING);
-	}
-
-	@Override
-	public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
-		return ClientHandler.RED_STEEL_CASING;
-	}
-
-	protected class MultiThermalCentrifugeRecipeLogic extends MultiblockRecipeLogic {
-
-		public MultiThermalCentrifugeRecipeLogic(RecipeMapMultiblockController tileEntity) {
-			super(tileEntity);
-		}
-
-		@Override
-		protected Recipe findRecipe(long maxVoltage, IItemHandlerModifiable inputs, IMultipleTankHandler fluidInputs) {
-			int currentItemsEngaged = 0;
-			int maxItemsLimit = 8;
-			int EUt = 0;
-			int duration = 0;
-			int currentTier = getOverclockingTier(maxVoltage);
-			int tierNeeded;
-			ArrayList<CountableIngredient> recipeInputs = new ArrayList<>();
-			ArrayList<ItemStack> recipeOutputs = new ArrayList<>();
-
-			for (int index = 0; index < inputs.getSlots(); index++) {
-				ItemStack stackInSlot = inputs.getStackInSlot(index);
-				if (stackInSlot.isEmpty())
-					continue;
-				Recipe matchingRecipe = recipeMap.findRecipe(maxVoltage, Collections.singletonList(stackInSlot), Collections.emptyList(), 0);
-				tierNeeded = Math.max(1, matchingRecipe == null ? Integer.MAX_VALUE : getOverclockingTier(matchingRecipe.getEUt()));
-				maxItemsLimit *= currentTier - tierNeeded;
-
-				EUt = matchingRecipe == null ? 0 : matchingRecipe.getEUt();
-				duration = matchingRecipe == null ? 0 : matchingRecipe.getDuration();
-
-				CountableIngredient inputIngredient = matchingRecipe == null ? null : matchingRecipe.getInputs().get(0);
-				if (inputIngredient != null && (maxItemsLimit - currentItemsEngaged) >= inputIngredient.getCount()) {
-					int overclockAmount = Math.min(stackInSlot.getCount() / inputIngredient.getCount(), (maxItemsLimit - currentItemsEngaged) / inputIngredient.getCount());
-					recipeInputs.add(new CountableIngredient(inputIngredient.getIngredient(), inputIngredient.getCount() * overclockAmount));
-					currentItemsEngaged += inputIngredient.getCount() * overclockAmount;
-					matchingRecipe.getOutputs().forEach(itemStack -> {
-						ItemStack outputStack = itemStack.copy();
-						if (!outputStack.isEmpty()) {
-							outputStack.setCount(outputStack.getCount() * overclockAmount);
-							recipeOutputs.add(outputStack);
-						}
-					});
-				}
-				if (currentItemsEngaged >= maxItemsLimit) break;
-			}
-
-			//merge recipe output because each input will need a output slot.
-			ArrayList<ItemStack> mergeOutput = recipeOutputs.stream().reduce(new ArrayList<>(), (ArrayList<ItemStack> itemStacks, ItemStack itemStack) -> {
-				Optional<ItemStack> result =  itemStacks.stream().filter(itemStack::isItemEqual).findFirst();
-				if(result.isPresent()){
-					result.get().grow(itemStack.getCount());
-				}else{
-					itemStacks.add(itemStack.copy());
-				}
-				return itemStacks;
-			}, (itemStacks, itemStacks2) -> {
-				itemStacks.addAll(itemStacks2);
-				return itemStacks;
-			});
+    @Override
+    public MetaTileEntity createMetaTileEntity(MetaTileEntityHolder holder) {
+        return new TileEntityLargeThermalCentrifuge(metaTileEntityId);
+    }
 
 
-			return recipeInputs.isEmpty() ? null : recipeMap.recipeBuilder()
-					.inputsIngredients(recipeInputs)
-					.outputs(mergeOutput)
-					.EUt((int) Math.max(1, EUt * 0.8))
-					//speed up by 150% mean divide by 2.5 but ... not sure if it's a thing implement in GT++
-					.duration((int) Math.max(1.0, duration * currentItemsEngaged / 2.5))
-					.build().getResult();
-		}
+    @Override
+    protected void formStructure(PatternMatchContext context) {
+        super.formStructure(context);
+        BlockWireCoil.CoilType coilType = context.getOrDefault("CoilType", BlockWireCoil.CoilType.CUPRONICKEL);
+    }
+
+    @Override
+    protected BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start()
+                .aisle("XXX", "XXX")
+                .aisle("XXX", "X#X")
+                .aisle("XXX", "XSX")
+                .setAmountAtLeast('L', 9)
+                .where('S', selfPredicate())
+                .where('L', statePredicate(getCasingState()))
+                .where('X', statePredicate(getCasingState()).or(abilityPartPredicate(ALLOWED_ABILITIES)))
+                .where('C', MetaTileEntityElectricBlastFurnace.heatingCoilPredicate())
+                .where('#', isAirPredicate())
+                .build();
+
+    }
+
+    public IBlockState getCasingState() {
+        return GAMetaBlocks.METAL_CASING.getState(GAMetalCasing.MetalCasingType.RED_STEEL_CASING);
+    }
+
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        return ClientHandler.RED_STEEL_CASING;
+    }
+
+    protected class MultiThermalCentrifugeRecipeLogic extends MultiblockRecipeLogic {
+
+        public MultiThermalCentrifugeRecipeLogic(RecipeMapMultiblockController tileEntity) {
+            super(tileEntity);
+        }
+
+        @Override
+        protected Recipe findRecipe(long maxVoltage, IItemHandlerModifiable inputs, IMultipleTankHandler fluidInputs) {
+            int maxItemsLimit = 8;
+            int EUt = 0;
+            int duration = 0;
+            int currentTier = getOverclockingTier(maxVoltage);
+            int tierNeeded;
+
+            int minMultiplier = Integer.MAX_VALUE;
+
+            Recipe matchingRecipe = recipeMap.findRecipe(maxVoltage, inputs, fluidInputs, 0);
+            if (matchingRecipe == null) {
+                return null;
+            }
+            tierNeeded = Math.max(1, getOverclockingTier(matchingRecipe.getEUt()));
+            maxItemsLimit *= currentTier - tierNeeded;
+
+            Set<ItemStack> countIngredients = new HashSet<>();
+            if (matchingRecipe.getInputs().size() != 0) {
+                this.findIngredients(countIngredients, inputs);
+                minMultiplier = Math.min(maxItemsLimit, this.getMinRatioItem(countIngredients, matchingRecipe, maxItemsLimit));
+            }
+
+            if (minMultiplier == Integer.MAX_VALUE) {
+                GTLog.logger.error("Cannot calculate ratio of items for processing array");
+                return null;
+            }
+
+            EUt = matchingRecipe.getEUt();
+            duration = matchingRecipe.getDuration();
+
+            List<CountableIngredient> newRecipeInputs = new ArrayList<>();
+            List<FluidStack> newFluidInputs = new ArrayList<>();
+            List<ItemStack> outputI = new ArrayList<>();
+            List<FluidStack> outputF = new ArrayList<>();
+            this.multiplyInputsAndOutputs(newRecipeInputs, newFluidInputs, outputI, outputF, matchingRecipe, minMultiplier);
 
 
-	}
+            return recipeMap.recipeBuilder()
+                    .inputsIngredients(newRecipeInputs)
+                    .outputs(outputI)
+                    .EUt((int) Math.max(1, EUt * 0.8))
+                    //speed up by 150% mean divide by 2.5 but ... not sure if it's a thing implement in GT++
+                    .duration((int) Math.max(1.0, duration * minMultiplier / 2.5))
+                    .build().getResult();
+        }
+
+
+        protected void findIngredients(Set<ItemStack> countIngredients, IItemHandlerModifiable inputs) {
+            for (int slot = 0; slot < inputs.getSlots(); slot++) {
+                ItemStack wholeItemStack = inputs.getStackInSlot(slot);
+                String name = wholeItemStack.getItem().getUnlocalizedNameInefficiently(wholeItemStack);
+                // skip empty slots
+                if (name.equals("tile.air")) {
+                    continue;
+                }
+                boolean found = false;
+                for (ItemStack i : countIngredients) {
+                    if (ItemStack.areItemsEqual(i, wholeItemStack)) {
+                        i.setCount(i.getCount() + wholeItemStack.getCount());
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    countIngredients.add(wholeItemStack.copy());
+                }
+            }
+        }
+
+        protected int getMinRatioItem(Set<ItemStack> countIngredients, Recipe r, int maxItemsLimit) {
+            int minMultiplier = Integer.MAX_VALUE;
+            for (CountableIngredient ci : r.getInputs()) {
+                if (ci.getCount() == 0) {
+                    continue;
+                }
+                for (ItemStack wholeItemStack : countIngredients) {
+                    if (ci.getIngredient().apply(wholeItemStack)) {
+                        int ratio = Math.min(maxItemsLimit, wholeItemStack.getCount() / ci.getCount());
+                        if (ratio < minMultiplier) {
+                            minMultiplier = ratio;
+                        }
+                        break;
+                    }
+                }
+            }
+            return minMultiplier;
+        }
+
+        protected void multiplyInputsAndOutputs(List<CountableIngredient> newRecipeInputs, List<FluidStack> newFluidInputs, List<ItemStack> outputI, List<FluidStack> outputF, Recipe r, int maxItemsLimit) {
+            for (CountableIngredient ci : r.getInputs()) {
+                CountableIngredient newIngredient = new CountableIngredient(ci.getIngredient(), ci.getCount() * maxItemsLimit);
+                newRecipeInputs.add(newIngredient);
+            }
+            for (FluidStack fs : r.getFluidInputs()) {
+                FluidStack newFluid = new FluidStack(fs.getFluid(), fs.amount * maxItemsLimit);
+                newFluidInputs.add(newFluid);
+            }
+            for (ItemStack s : r.getOutputs()) {
+                int num = s.getCount() * maxItemsLimit;
+                ItemStack itemCopy = s.copy();
+                itemCopy.setCount(num);
+                outputI.add(itemCopy);
+            }
+            for (FluidStack f : r.getFluidOutputs()) {
+                int fluidNum = f.amount * maxItemsLimit;
+                FluidStack fluidCopy = f.copy();
+                fluidCopy.amount = fluidNum;
+                outputF.add(fluidCopy);
+            }
+        }
+    }
 }
